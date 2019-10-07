030c0c1ee8fd1f7552a9db5173cc2e0d1c5119bc
Ivan <Kishkan91@gmail.com>
2019-10-07 17:51:58 +0400

    profiles in progress

--- src/main/java/com/kishkan/epam/Application.java
+++ src/main/java/com/kishkan/epam/Application.java
@@ -2,6 +2,8 @@ package com.kishkan.epam;
 
 public class Application {
     public static void main(String[] args) {
+       // String path = pro
+        //https://devcolibri.com/maven-%D1%81%D0%BE%D0%B7%D0%B4%D0%B0%D0%BD%D0%B8%D0%B5-profiles/
 
     }
 }
