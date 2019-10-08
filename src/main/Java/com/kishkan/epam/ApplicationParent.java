2d28d7f0e6dd630b9954396f3517a05cbf0a5a94
Ivan <Kishkan91@gmail.com>
2019-10-08 17:42:50 +0400

    Maven pain still in progress

---
+++ src/main/Java/com/kishkan/epam/ApplicationParent.java
@@ -0,0 +1,42 @@
+package com.kishkan.epam;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ApplicationParent {
+    public static void main(String[] args) {
+        ApplicationParent app = new ApplicationParent();
+        Properties prop = app.loadPropertiesFile("config.properties");
+        System.out.println(prop.getProperty("path"));
+//        System.out.println(app.getPath());
+    }
+
+    public Properties loadPropertiesFile(String filePath) {
+
+        Properties prop = new Properties();
+
+        try (InputStream resourceAsStream = getClass().getClassLoader().getResourceAsStream(filePath)) {
+            prop.load(resourceAsStream);
+        } catch (IOException e) {
+            System.err.println("Unable to load properties file : " + filePath);
+        }
+
+        return prop;
+
+    }
+
+//    private String getPath() {
+//        try (InputStream input = new FileInputStream("src/main/resources/config.properties")) {
+//            Properties prop = new Properties();
+//            prop.load(input);
+//
+//            return  prop.getProperty("path");
+//
+//        } catch (IOException ex) {
+//            ex.printStackTrace();
+//        }
+//
+//        return "не нашел файл";
+//    }
+}
